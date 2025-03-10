d1d96bf1d315bfbb68ad71e8d0cd2c0241a060d2
Pratham Jain <2muchappy420@gmail.com>
2025-03-11 00:32:48 +0530

    Otp and user verification done

---
+++ mystery/src/app/api/check-username-unique/route.ts
@@ -0,0 +1,68 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { usernameValidation } from "@/schemas/signUpSchema";
+import {z} from "zod";
+
+
+const UsernameQuerySchema = z.object({
+    username: usernameValidation
+}) 
+
+export async function GET(request: Request) {
+
+    await dbConnect();
+    
+    try {
+        const {searchParams} = new URL(request.url)
+        const queryParam = {
+            username: searchParams.get("username")
+        }
+
+        //validate with zod
+        const result = UsernameQuerySchema.safeParse(queryParam);
+        console.log("Do read this validation from zod X-D", result);
+
+        if (!result.success) {
+            const usernameErrors = result.error.format().username?._errors || [];
+            return Response.json(
+                {
+                    success: false,
+                    message: usernameErrors?.length > 0 ? usernameErrors.join(", ") : "Invalid query param"
+                },
+                { status: 400 }
+            )
+        }
+
+        const {username} = result.data;
+        
+        const existingVerifiedUser = await UserModel.findOne({ username, isVerified: true });
+
+        if (existingVerifiedUser) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "Username already taken"
+                },
+                { status: 400 }
+            )
+        }
+
+        return Response.json(
+            {
+                success: true,
+                message: "Username is unique"
+            },
+            { status: 201 }
+        )
+
+    } catch (error) {
+        console.error("Error checking username", error);
+        return Response.json(
+            {
+                success: false,
+                message: "Error checking username"
+            },
+            { status: 500 }
+        )
+    }
+}
