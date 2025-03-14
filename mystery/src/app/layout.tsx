e074f9184d134f250aa1eac2ff56663e43d64d33
Pratham Jain <2muchappy420@gmail.com>
2025-03-14 23:51:39 +0530

    Added navbar and card components

--- mystery/src/app/layout.tsx
+++ mystery/src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import AuthProvider from "@/context/AuthProvider";
+import { Toaster } from "@/components/ui/sonner"
 import Navbar from "@/components/Navbar";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,15 +27,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      
+      <AuthProvider>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar /> 
         {children}
 
+        <Toaster />
+
       </body>
 
+      </AuthProvider>
     </html>
   );
-}
+};
